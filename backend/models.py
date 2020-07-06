83d3d7ac4af514be30b486c3e3b248851d2b7531
gouravgoel <67143979+gouravgoel@users.noreply.github.com>
2020-07-06 16:30:35 +0530

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -8,10 +8,10 @@ database_path = 'postgresql://postgres:0129@localhost:5432/trivia'
 
 db = SQLAlchemy()
 
-#-----------------------------------------------------------------------------#
+# -----------------------------------------------------------------------#
 # setup_db(app)
 # binds a flask application and a SQLAlchemy service
-#-----------------------------------------------------------------------------#
+# -----------------------------------------------------------------------#
 
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
@@ -20,60 +20,60 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
-#-----------------------------------------------------------------------------#
+# -----------------------------------------------------------------------#
 # Question
-#-----------------------------------------------------------------------------#
+# -----------------------------------------------------------------------#
 
-class Question(db.Model):  
-  __tablename__ = 'questions'
+class Question(db.Model):
+    __tablename__ = 'questions'
 
-  id = Column(Integer, primary_key=True)
-  question = Column(String)
-  answer = Column(String)
-  category = Column(String)
-  difficulty = Column(Integer)
+    id = Column(Integer, primary_key=True)
+    question = Column(String)
+    answer = Column(String)
+    category = Column(String)
+    difficulty = Column(Integer)
 
-  def __init__(self, question, answer, category, difficulty):
-    self.question = question
-    self.answer = answer
-    self.category = category
-    self.difficulty = difficulty
+    def __init__(self, question, answer, category, difficulty):
+        self.question = question
+        self.answer = answer
+        self.category = category
+        self.difficulty = difficulty
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    def update(self):
+        db.session.commit()
 
-  def format(self):
-    return {
-      'id': self.id,
-      'question': self.question,
-      'answer': self.answer,
-      'category': self.category,
-      'difficulty': self.difficulty
-    }
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
-#-----------------------------------------------------------------------------#
+    def format(self):
+        return {
+            'id': self.id,
+            'question': self.question,
+            'answer': self.answer,
+            'category': self.category,
+            'difficulty': self.difficulty
+        }
+
+# -----------------------------------------------------------------------#
 # Category
-#-----------------------------------------------------------------------------#
+# -----------------------------------------------------------------------#
 
-class Category(db.Model):  
-  __tablename__ = 'categories'
+class Category(db.Model):
+    __tablename__ = 'categories'
 
-  id = Column(Integer, primary_key=True)
-  type = Column(String)
+    id = Column(Integer, primary_key=True)
+    type = Column(String)
 
-  def __init__(self, type):
-    self.type = type
+    def __init__(self, type):
+        self.type = type
 
-  def format(self):
-    return {
-      'id': self.id,
-      'type': self.type
-    }
+    def format(self):
+        return {
+            'id': self.id,
+            'type': self.type
+        }
